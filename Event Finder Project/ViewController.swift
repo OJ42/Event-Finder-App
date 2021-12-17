911f3a5a630852740f50d86ce3a74bc48b5b380f
david <mcdavid42@gmail.com>
2021-12-17 01:50:19 -0700

    Initial Commit

---
+++ Event Finder Project/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Event Finder Project
+//
+//  Created by David McCoy on 12/17/21.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
